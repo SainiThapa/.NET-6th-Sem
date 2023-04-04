226ede9c16fc46d21d05f0466d90f98b1076b34b
Saini Thapa <magarprisu4747@gmail.com>
2023-04-04 08:11:46 +0545

    Delegate and Async method

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class Program
+    {
+        delegate int Sum(int x, int y);
+        delegate int Product(int x, int y);
+        static int calculate1(int x, int y)
+        {
+            return x+y;
+        }
+        static int calculate2(int x, int y)
+        {
+            return x * y;
+        }
+        delegate void myDelegate(Program1 pro);
+
+        static void myFunction(Program1 obj1)
+        {
+            obj1.mymethod(1,2);
+        }
+        static void Main(string[] args)
+        {
+/*            //DELEGATE INT
+            Sum p1 = calculate1;
+            Product d1 = calculate2;
+            Console.WriteLine("Enter two numbers to perform addition and multiplication: ");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int c = Convert.ToInt32(a);
+            int d = Convert.ToInt32(b);
+            int result1 = p1(c, d);
+            int result2 = d1(c, d);
+            Console.WriteLine("Sum = " + result1);
+            Console.WriteLine("Product = " + result2);/*/
+
+            //DELEGATE CLASSOBJ
+            Program1 obj1 = new Program1();
+            myDelegate D = myFunction;
+            D(obj1);
+        }
+    }
+}
---
+++ Delegate/Delegate/Program1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class Program1
+    {
+        public void mymethod(int x, int y)
+        {
+            Console.WriteLine("mymethod called with {0} and {1} ", x, y);
+
+        }
+    }
+}

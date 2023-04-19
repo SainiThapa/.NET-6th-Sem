170d9a85f233493158710eee31123f20c900623c
Saini Thapa <magarprisu4747@gmail.com>
2023-04-19 21:32:48 +0545

    dynamic List in file

---
+++ MVC_01_05/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_01_05.Models;
+
+namespace MVC_01_05.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult College()
+        {
+            List<CollegeModel> colleges = new List<CollegeModel>();
+            StreamReader sr = new StreamReader("wwwroot/college.txt");
+            string wline=sr.ReadLine();
+            while (wline != null)
+            {
+                string[] collegeDetails = wline.Split(",");
+                CollegeModel college= new CollegeModel();
+                college.collegename = collegeDetails[0];
+                college.Address = collegeDetails[1];
+                college.Universityname = collegeDetails[2];
+                colleges.Add(college);
+                wline = sr.ReadLine();
+            }
+            return View(colleges);
+        }
+        public ActionResult AddCollege()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Createcollege(CollegeModel college)
+        {
+            string datatoSave = college.collegename + "," + college.Address + "," + college.Universityname;
+            StreamWriter sw = new StreamWriter("wwwroot/college.txt",true);
+            sw.WriteLine(datatoSave);
+            sw.Close();
+            return RedirectToAction("College");
+        }
+    }
+}
---
+++ MVC_01_05/Models/CollegeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_01_05.Models
+{
+    public class CollegeModel
+    {
+        public string collegename { get; set; }
+        public string Address { get; set; }
+        public string Universityname { get; set; }
+    }
+}

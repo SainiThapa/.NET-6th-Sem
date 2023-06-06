803233e0ee9141578cd4fa8ff05f17341615f6ac
Saini Thapa <magarprisu4747@gmail.com>
2023-06-06 07:23:49 +0545

    Migration

---
+++ MVC_02_22/EntityData/EntityDB.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_02_22.EntityData
+{
+    public class EntityDB:DbContext
+    {
+        public EntityDB(DbContextOptions<EntityDB> options) : base(options) { 
+        }
+
+        DbSet<Student> Students { get; set; }
+        DbSet<TextReader> Teachers { get; set; }
+
+    }
+}
---
+++ MVC_02_22/EntityData/Student.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_02_22.EntityData
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(50")]
+        public string Name { get; set; }
+        
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Address { get; set; }
+        [Required]
+        public string Contact{ get; set; }
+    }
+    public class Teacher
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName ="varchar(50)")]
+        public string TeacherName { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+
+    }
+
+}
